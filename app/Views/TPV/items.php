9896f0b96937cdad5455c0b89d826aeb638590f9
alejandro23dev <alejandro23dev@gmail.com>
2024-08-26 09:47:41 -0400

    upd

--- app/Views/TPV/items.php
+++ app/Views/TPV/items.php
@@ -20,8 +20,18 @@
 						<td class="dt-vertical-align p-2 fs-7 text-primary">
 							<div class="row d-flex align-items-center">
 								<div class="col-4 ms-auto d-flex align-items-center">
-									<a href="#" class="edit-price pe-4" data-invoice-items-id="<?php echo $item->id; ?>" data-service-info="<?php echo getService($item->service_id)[0]->name . ' (' . getMoneyFormat($config[0]->currency, $item->amount) . ')'; ?>"><i class="fas fa-pen-square text-warning fs-5"></i></a>
-									<a href="#" class="delete-service pe-4" data-invoice-items-id="<?php echo $item->id; ?>" data-service-id="<?php echo $item->service_id; ?>"><i class="fas fa-trash text-danger fs-5"></i></a>
+									<a href="#" class="edit-price pe-4" data-invoice-items-id="<?php echo $item->id; ?>" data-service-info="<?php echo getService($item->service_id)[0]->name . ' (' . getMoneyFormat($config[0]->currency, $item->amount) . ')'; ?>">
+										<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+											<path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+											<path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
+										</svg>
+									</a>
+									<a href="#" class="delete-service pe-4" data-invoice-items-id="<?php echo $item->id; ?>" data-service-id="<?php echo $item->service_id; ?>">
+										<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+											<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+											<path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+										</svg>
+									</a>
 								</div>
 								<div class="col-4 ms-auto">
 									<div class="position-relative d-flex align-items-center" data-kt-dialer="true" data-kt-dialer-min="1" data-kt-dialer-max="10" data-kt-dialer-step="1" data-kt-dialer-decimals="0">
